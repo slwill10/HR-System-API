76fb033bb46d1e66fb25cc928f5b677c4668644a
Willian Santos <wsantosdelima44@gmail.com>
2024-07-02 10:02:14 -0300

    Criando classe 'CustomUserDetailsService'

---
+++ src/main/java/com/example/HR_system1/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.HR_system1.infra.security;
+
+import com.example.HR_system1.domain.user.User;
+import com.example.HR_system1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
